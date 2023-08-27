9e23e7dcaf1b4ecaf7879ee7869381de29e9cd35
bruhge <49010797+bruhge@users.noreply.github.com>
2023-08-26 22:39:09 -0500

    Add files via upload
    changed allowed hosts

--- rsquared/rsquared/settings.py
+++ rsquared/rsquared/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ditf0mkimu5vq!m7vork$8a$9jmrdl**6avyd724)+t)jplc42
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
