ed538edfeece974d1a91021bed56932705f6fb37
bruhge <49010797+bruhge@users.noreply.github.com>
2026-07-02 06:43:57 -0500

    Added contact form and page
    First attempt at css for splash screen when form is submitted.  Added contact form, contact.html and seperated index.html into base.html so contact.html can use header/footer from index.html

--- rsquared/main/views.py
+++ rsquared/main/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        subject = f'Query from {name}'
+        body = f'Email from {email},\n\n{message}'
+        send_mail(
+            subject=subject,
+            message=body,
+            from_email='bruhge@gmail.com',
+            recipient_list=['bruhge@gmail.com'],
+            fail_silently=False
+        )
+        messages.success(request, 'Thank you for submitting!  Your message has been sent.')
+        return redirect('home')
+    return render(request, 'main/contact.html')
--- rsquared/rsquared/settings.py
+++ rsquared/rsquared/settings.py
@@ -127,3 +127,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
