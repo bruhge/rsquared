8f59eb8cfc58987ff1d25fb4032ac5a3711b0940
bruhge <49010797+bruhge@users.noreply.github.com>
2026-07-03 08:36:26 -0500

    Added pages
    Added evaluations.html, modalities.html, specialities.html, and therapy.html.  Updated viewsm urls, base.html, and css to match new pages

--- rsquared/main/urls.py
+++ rsquared/main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, contact
+from .views import index, contact, evaluations, therapy, speciality, modalities
 
 urlpatterns = [
     path('', index, name='home'),
     path('contact/', contact, name='contact'),
+    path('evaluations/', evaluations, name='evaluations'),
+    path('therapy/', therapy, name='therapy'),
+    path('speciality/', speciality, name='speciality'),
+    path('modalities/', modalities, name='modalities'),
 ]
--- rsquared/main/views.py
+++ rsquared/main/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'active': 'home'})
 
 def contact(request):
     if request.method == 'POST':
@@ -23,4 +23,16 @@ def contact(request):
         )
         messages.success(request, 'Thank you for submitting!  Your message has been sent.')
         return redirect('home')
-    return render(request, 'main/contact.html')
+    return render(request, 'main/contact.html', {'active': 'contact'})
+
+def evaluations(request):
+    return render(request, 'main/evaluations.html', {'active': 'evaluations'})
+
+def therapy(request):
+    return render(request, 'main/therapy.html', {'active': 'therapy'})
+
+def speciality(request):
+    return render(request, 'main/specialities.html', {'active': 'specialities'})
+
+def modalities(request):
+    return render(request, 'main/modalities.html', {'active': 'modalities'})
